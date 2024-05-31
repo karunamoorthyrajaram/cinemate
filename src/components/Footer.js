05a1d64cd94104e87971126ab1938c0af0710fcd
karunamoorthyrajaram <karunamoorthy098@gmail.com>
2024-05-31 20:30:38 +0530

    updated H&F

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export const Footer = () => {
     
 
 <footer className="bg-white shadow dark:bg-gray-800">
-    <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+    <div className="w-full mx-auto p-4 md:flex md:items-center md:justify-between">
       <span className="text-sm  text-gray-500 sm:text-center dark:text-gray-400">© 2030 <Link to="/" className="hover:underline">CineMate™</Link>. All Rights Reserved.
     </span>
     <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
--- src/components/Header.js
+++ src/components/Header.js
@@ -20,7 +20,7 @@ export const Header = () => {
   return (
     <header>
       <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5  dark:bg-gray-900">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto">
+        <div className="flex flex-wrap items-center justify-between mx-auto">
         <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
             <img src={Logo} className="h-8 rounded-lg" alt="cinemate Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">CineMate</span>
